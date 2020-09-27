8aec3214d3c39395b8a7f89d6d8c48e481a8bb32
Ting Guo <guoting.hust@gmail.com>
2020-09-27 00:27:24 -0700

    add localStorage to save the contents

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-const savedMovies = (localStorage.getItem("movies")) ?
+const savedMoviesArr = (localStorage.getItem("movies")) ?
     JSON.parse(localStorage.getItem("movies")) : [];
 
-for (let movie of savedMovies) {
-    makeList(todo.text);
-    if (todo.crossed) {
-        ul.lastChild.style.textDecoration = "line-through";
-    }
+for (let movie of savedMoviesArr) {
+    addMovie(movie[0], movie[1]);
 }    
 
+//function to add movie with rating and remove button
+function addMovie(titleVal, ratingVal) {
+    $("#displayTable").append("<tr></tr>");
+    $("#displayTable tr").last().append(`<td>${titleVal}</td>`)
+        .append(`<td>${ratingVal}</td>`)
+        .append(`<td><button>Remove</button></td>`);    
+}
 
+// add event listener for submit button to add new movie to the DOM
 $("form").on("submit", function(e){
     e.preventDefault();
-    $("#displayTable").append(`<tr></tr>`);
-    $("#displayTable tr").last().append(`<td>${$("#title").val()}</td>`)
-                                .append(`<td>${$("#rating").val()}</td>`)
-                                .append(`<td><button>Remove</button></td>`);
+    addMovie($("#title").val(), $("#rating").val());  
+    savedMoviesArr.push([$("#title").val(), $("#rating").val()]); 
+    //update localStorage 
+    localStorage.setItem("movies", JSON.stringify(savedMoviesArr));
     $("input").val("");
 })
 
+// add event listener for remove button (delegatioin)
 $("#displayTable").on("click", "button", function(e){
+    let clickedLiIndex = $("#displayTable button").index(e.target);
+    savedMoviesArr.splice(clickedLiIndex, 1);
+    localStorage.setItem("movies", JSON.stringify(savedMoviesArr));
     $(e.target).parent().parent().remove();
 })
 
+// add event listener for the sort select options 
 $("#sort").on("click", function(e){
     if ($(e.target).val() === "titleA-Z") {
         $("#tHead").siblings().sort(function(a,b){
@@ -29,6 +39,8 @@ $("#sort").on("click", function(e){
             let $b = $(b).children("td:first").text();
             return ($a > $b) ? 1 : (($a < $b) ? -1 : 0); 
         }).appendTo("#displayTable");
+        // sort savedMoviesArr to update the localStorage
+        savedMoviesArr.sort((x, y) => (x[0] > y[0]) ? 1 : (x[0] < y[0]) ? -1 : 0);
     }
     else if ($(e.target).val() === "titleZ-A") {
         $("#tHead").siblings().sort(function (a, b) {
@@ -36,6 +48,7 @@ $("#sort").on("click", function(e){
             let $b = $(b).children("td:first").text();
             return ($a < $b) ? 1 : (($a > $b) ? -1 : 0);
         }).appendTo("#displayTable");
+        savedMoviesArr.sort((x, y) => (x[0] < y[0]) ? 1 : (x[0] > y[0]) ? -1 : 0);
     }
     else if ($(e.target).val() === "ratingL-H") {
         $("#tHead").siblings().sort(function (a, b) {
@@ -43,6 +56,7 @@ $("#sort").on("click", function(e){
             let $b = $(b).children("td:nth-child(2)").text();
             return ($a > $b) ? 1 : (($a < $b) ? -1 : 0);
         }).appendTo("#displayTable");
+        savedMoviesArr.sort((x, y) => x[1] - y[1]);
     }
     else if($(e.target).val() === "ratingH-L") {
         $("#tHead").siblings().sort(function (a, b) {
@@ -50,6 +64,9 @@ $("#sort").on("click", function(e){
             let $b = $(b).children("td:nth-child(2)").text();
             return ($a < $b) ? 1 : (($a > $b) ? -1 : 0);
         }).appendTo("#displayTable");
+        savedMoviesArr.sort((x, y) => y[1] - x[1]);
     }
+
+    localStorage.setItem("movies", JSON.stringify(savedMoviesArr));
 })
 
